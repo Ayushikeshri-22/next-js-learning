05b68c3940101f8c030f1080270e8ccf407db156
Ayushi Keshri <akeshri@victoria.com>
2025-03-11 14:42:38 +0530

    custom not found page

---
+++ my-app/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,3 @@
+export default function NotFound(){
+	return <h1> Not Found custom page for only review</h1>
+}
--- my-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ my-app/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,14 @@
+import { notFound } from "next/navigation"
+
 export default function ReviewDetails({ params } :
 	{params : {
 		reviewId : string,
 		productId : string
 	}}
  ) {
+	if(parseInt(params.reviewId) > 1000){
+		notFound()
+	}
 
 	return <h1> Review of {params.reviewId} of product {params.productId}</h1>
 }
