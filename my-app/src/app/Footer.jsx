40666d59ce88e9ee68019938aee6d31b638bddc1
Ayushi Keshri <akeshri@victoria.com>
2025-03-21 19:31:07 +0530

    layout

---
+++ my-app/src/app/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+	return (
+	  <footer
+		style={{
+		  backgroundColor: 'lightpink',
+		  padding: "1rem",
+		}}
+	  >
+		<p>Footer component</p>
+	  </footer>
+	);
+  }
---
+++ my-app/src/app/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header
+      style={{
+        backgroundColor: 'lightyellow',
+        padding: "1rem",
+      }}
+    >
+      <p>header component</p>
+    </header>
+  );
+}
