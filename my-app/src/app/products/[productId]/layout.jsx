b57361b93473552aea708682413cfc494ae4d99b
Ayushi Keshri <akeshri@victoria.com>
2025-03-21 19:37:40 +0530

    nested layout

---
+++ my-app/src/app/products/[productId]/layout.jsx
@@ -0,0 +1,10 @@
+export default function ProductLayout({children}) {
+  return (
+	<div style={{backgroundColor:'lightcyan'}}>
+	  {children}
+	  <div>
+		Carousel of all the products here
+	  </div>
+	</div>
+  );
+}
