f2a685c8b1c5b5e2609c8bcaa43960c191f05f34
Ayushi Keshri <akeshri@victoria.com>
2025-03-28 01:36:18 +0530

    meta title as object

--- my-app/src/app/blog/page.tsx
+++ my-app/src/app/blog/page.tsx
@@ -1,3 +1,13 @@
+import { Metadata } from "next"
+
+
+export const metadata: Metadata = {
+	title: {
+		absolute: 'absolute title',
+		template: `Blog`,
+	}
+}
+
 export default function Blog(){
 	return <h1> blog page</h1>
 }
