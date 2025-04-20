5d1abfbb3657d32c2d6d6267cd52eace6061b3e7
Ayushi Keshri <akeshri@victoria.com>
2025-04-20 19:55:58 +0530

    added linking to the pages

--- food-app/app/meals/page.js
+++ food-app/app/meals/page.js
@@ -1,5 +1,11 @@
+import Link from "next/link"
+
 export default function Meal(){
 	return (
-		<div>Meal page</div>
+		<div>
+			<h1>Meal page </h1>
+			<br/>
+			<Link href="/meals/share" style={{color:'blue'}}> Share</Link>
+		</div>
 	)
 }
--- food-app/app/page.tsx
+++ food-app/app/page.tsx
@@ -1,9 +1,13 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <main> Lets start</main>
+      <main>
+        <h1>Main Page</h1> <br/>
+        <Link href="/meals" style={{color:'blue'}}> Meal </Link><br/>
+        <Link href="/community" style={{color:'blue'}}> Community </Link>
+      </main>
     </div>
   );
 }
